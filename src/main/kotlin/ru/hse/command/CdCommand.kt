3ed58b49a15cc30114fcaf27c485c8950eed0f96
vladnosiv <vladnosiv@gmail.com>
2022-03-06 14:20:17 +0300

    implement cd command

---
+++ src/main/kotlin/ru/hse/command/CdCommand.kt
@@ -0,0 +1,41 @@
+package ru.hse.command
+
+import ru.hse.environment.Environment
+import ru.hse.executable.Executable
+import ru.hse.executable.ExecutionResult
+import ru.hse.utils.writeln
+import java.io.InputStream
+import java.io.OutputStream
+import kotlin.io.path.*
+
+/**
+ * cd [arg ] -- меняет текущую рабочую директорию
+ * Если аргумента нет -- меняет директорию на домашнюю для пользователя
+ * Если аргумент есть -- меняет рабочую директорию на новую
+ */
+class CdCommand(
+    private val environment: Environment,
+    private val args: List<String>
+) : Executable {
+    override fun run(input: InputStream, output: OutputStream, error: OutputStream
+    ): ExecutionResult = when(args.size) {
+        0 -> {
+            environment.workDirectory = Path(System.getProperty("user.home"))
+            ExecutionResult.success
+        }
+        1 -> when (Path(args[0]).isDirectory()) {
+                true -> {
+                    environment.workDirectory = Path(args[0])
+                    ExecutionResult.success
+                }
+                false -> {
+                    error.writeln("cd: ${args[0]} is not a directory")
+                    ExecutionResult.fail
+                }
+            }
+        else -> {
+            error.writeln("cd: too many arguments")
+            ExecutionResult.fail
+        }
+    }
+}
